a256704d15bacb2556bb92567eff1fb755bc8cad
Jake <jakewarrenblack01@gmail.com>
2022-10-25 11:20:59 +0100

    Update user_controller and user route, these weren't updated properly in previous commit

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,76 +1,54 @@
+const User = require('../models/user_schema');
+const bcrypt = require('bcryptjs');
 
-const readData = (req, res) => {
-    res.status(200).json({
-        "msg" : "All users retrieved"
+const register = (req, res) => {
+    let newUser = new User(req.body);
+    newUser.password = bcrypt.hashSync(req.body.password, 10);
+
+    console.log(newUser);
+
+    newUser.save((err, user) => {
+        if(err){
+            return res.status(400).json({
+                msg: err
+            });
+        }
+        else {
+            user.password = undefined;
+            return res.status(201).json(user);
+        }
     });
+
+
 };
 
-const readOne = (req, res) => {
+const login = (req, res) => {
+    User.findOne({
+        email: req.body.email
+    })
+    .then((user) => {
 
-    let id = req.params.id;
-
-    // connect to db and retrieve user with :id
-
-    res.status(200).json({
-        "msg" : `You retrieved user with ID: ${id}`
-    });
+        if(!user || !user.comparePassword(req.body.password)){
+            res.status(401).json({
+                msg: 'Authentication failed. Invalid user or password'
+            });
+        }
+        else {
+            // generate a token
+            res.status(200).json({
+                msg: 'All good'
+            });
+        }
+    })
+    .catch((err) => {
+        throw err;
+    })
 };
 
-const createData = (req, res) => {
-    console.log(req.body);
-    let data = req.body;
-
-    // connect to db, check if email exists, if yes respond with error
-    // if some user info is missing, respond with error
-
-    if(data.password.length < 6){
-        res.status(422).json({
-            "msg": "User password must be over 6 characters"
-        });
-    }
-    else{
-        data.id = 1;
-        res.status(201).json({
-            "msg": "All good",
-            "data": data
-        });
-    }
-    
-};
-
-const updateData = (req, res) => {
-
-    let id = req.params.id;
-    let data = req.body;
-    
-    // connect to db and retrieve user with :id
-    // if user exists, validate the new user info, if all good update user
-
-    data.id = id;
-
-    res.status(200).json({
-        "msg" : `You edited user with ID: ${id}`,
-        "data" : data
-    });
-};
-
-const deleteData = (req, res) => {
-
-    let id = req.params.id;
-
-    // connect to db and retrieve user with :id and delete them
-
-    res.status(200).json({
-        "msg" : `You deleted user with ID: ${id}`
-    });
-};
 
 module.exports = {
-    readData,
-    readOne,
-    createData,
-    updateData,
-    deleteData
+    register,
+    login
 };
 
 
--- routes/users.js
+++ routes/users.js
@@ -2,18 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const { 
-    readData, 
-    readOne,
-    createData,
-    updateData,
-    deleteData
+    register, 
+    login
   } = require('../controllers/user_controller');
 
 router
-    .get('/', readData)
-    .get('/:id', readOne)
-    .post('/', createData)
-    .put('/:id', updateData)
-    .delete('/:id', deleteData);
+    .post('/register', register)
+    .post('/login', login)
 
 module.exports = router;
