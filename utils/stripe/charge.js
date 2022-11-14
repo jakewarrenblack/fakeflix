69c2a272021f5acfc577d5cc16023aeb8e271bb2
jakewarrenblack <jake.black@sonalake.com>
2022-11-14 22:35:11 +0000

    Stripe Integration: -  Added 'stripe_details' field of generic type to user schema and ignored this field in faker. - Once a new user is registered, stripe key, price of their chosen plan, and email are passed in query params and the 'Home' page is rendered through an EJS template - The query params are passed into hidden fields. Within the template is a form which calls the stripe payment form script. - When the form is submitted, the stripe data and query params from the user are passed through to the /charge endpoint, and the charge() method runs. - We then find the user we registered earlier, creating a stripe customer object based on its data, along with the payment token from the form. - The user is then updated to contain its stripe_details, which were empty when we started. - With the customer object made and the user object updated, we then create a stripe charge, which will appear on the stripe dashboard.

---
+++ utils/stripe/charge.js
@@ -0,0 +1,63 @@
+require('dotenv').config({path: "../../.env"})
+const Stripe = require('stripe');
+const stripe = Stripe(process.env.STRIPE_API_KEY);
+const {model: User} = require("../../models/user_schema");
+
+const viewProfile = async (email) => {
+    return await User.find({email: email})
+        .then((data) => {
+            if (data) {
+                return data
+            }
+        })
+}
+
+const createCustomer = async (data) => {
+    // First I find the profile with the email matching the one entered in the form
+    return await viewProfile(data.email).then(async (userRes) => {
+        userRes = userRes[0]
+        // Create a stripe user from this email, passing the transaction token from the form
+        return await stripe.customers.create({
+            name: `${userRes.firstName} ${userRes.lastName}`,
+            email: data.email,
+            source: data.token
+        }).then(async (stripeRes) => {
+            // stripe_details object will so far be empty, having been left with a type of 'any' and ignored by faker
+            return await User.findOneAndUpdate({email: data.email}, {stripe_details: {...stripeRes}}, {
+                new: true,
+            })
+                .then((data) => {
+                    if (data) {
+                        // Updated the customer object to include the stripe data
+                        return data;
+                    }
+                })
+        })
+    }).then((res) => res)
+        .catch(e => console.log(e))
+}
+
+
+const charge = async (req, res) => {
+    let token = req.body.stripeToken
+    let {price, subscription_type, email} = req.body;
+    await createCustomer({
+        email: email,
+        token: token
+    }).then(async (response) => {
+        let stripeDetails = response.stripe_details
+
+        await stripe.charges.create({
+            // Doesn't support floats and receives value in cents
+            amount: parseInt(price) * 100,
+            currency: 'eur',
+            description: `${subscription_type}`,
+            customer: stripeDetails.id
+        }).then((res) => res)
+            .catch((e) => e)
+    }).catch((e) => e)
+
+
+}
+
+module.exports = {charge, createCustomer}
